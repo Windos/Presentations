29ff529ffd72ed6b815dcfe5d579bfd8816b0d53
Windos <windosbk+github@gmail.com>
2018-12-12 08:01:43 +1300

    MSPSUG December

---
+++ MSPSUG December 2018 Speed/landing.ps1
@@ -0,0 +1,26 @@
+
+<#
+
+    11 concepts, 16 demos + PS Core bonus round
+
+    Demos may have unexpected results (and that's fine)
+
+    Numerous VS Code & PowerShell tools being used
+        If you're interested in the mechanics let's chat afterwards
+
+    You won't see me run any code, that's happening automatically
+
+#>
+
+#region Example Setup
+Add-Type -Path 'C:\temp\Selenium\WebDriver.dll'
+
+$Global:Selenium = New-Object OpenQA.Selenium.Chrome.ChromeDriver
+$Url = 'http://localhost'
+
+Start-RunBucket -Control {Start-Sleep -Milliseconds 5} -Variation {} -Title 'Example'
+
+$Global:Selenium.Navigate().GoToUrl($Url)
+
+$ErrorActionPreference = 'SilentlyContinue'
+#endregion
